0be184366128918cffe6ac83b3805459061ba661
Madalena PAVAO <madalenapavao@gmail.com>
2020-08-03 11:55:23 +0200

    Created empty components: navbar, home-page, login and food-catalogue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { FoodCatalogueComponent } from './food-catalogue/food-catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    LoginComponent,
+    FoodCatalogueComponent
   ],
   imports: [
     BrowserModule,
