32318dbb2910f08d9be2ce15078042ac57218f41
jerno <jerno@me.com>
2020-08-03 14:19:12 +0200

    Split users service to Users and Cart

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from "rxjs/operators"
-import { Subject, Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   users: any;
-  user: Subject<any> = new Subject();
+  public user: Subject<any> = new Subject();
 
   constructor(private _httpClient : HttpClient) {
     const localUsers = JSON.parse(localStorage.getItem("users"));
@@ -53,139 +53,6 @@ export class UsersService {
   logout() {
     this.user.next(null);
     localStorage.setItem("isConnected", "false");
-  }
-
-  addProduct(product: any, quantite: number, repasLibelle?: number) {
-     return new Observable(observer => {
-      this.user.subscribe(user => {
-        const updatedUser = JSON.parse(JSON.stringify(user));
-        
-        if(!updatedUser.commandeList.length) {
-          observer.error("Cannot add product. Order doesnt exists");
-          return
-        }
-
-        const command = updatedUser.commandeList[0];
-
-        const productItem = {
-            id: this.generateId(),
-            quantite: quantite,
-            repasLibelle: repasLibelle,
-            produit: product
-        };
-
-        const productFound = command.ligneCommandeList.find(p => p.id === product.id);
-        if (productFound) {
-          productFound.quantite++;
-        } else {
-          command.ligneCommandeList.push(productItem);
-        }
-
-        this.updatePriceTotal(command);
-
-        this.user.next(updatedUser);
-        observer.next(updatedUser);
-        observer.complete();
-      }, error => {
-        observer.error("Cannot add product");
-      });
-    });
-  }
-
-  generateId() {
-    return (new Date()).getTime();
-  }
-
-  createOrder(order: {date: Date, addressShiping?: string, addressInvoice: string}) {
-    return new Observable(observer => {
-      this.user.subscribe(user => {
-        const updatedUser = JSON.parse(JSON.stringify(user));
-        updatedUser.commandeList.push(order);
-        this.user.next(updatedUser);
-        observer.next(updatedUser);
-        observer.complete();
-      }, error => {
-        observer.error("Cannot create order");
-      });
-    });
-  }
-
-  removeProduct(id: number) {
-    return new Observable(observer => {
-      this.user.subscribe(user => {
-        const updatedUser = JSON.parse(JSON.stringify(user));
-        
-        if(!updatedUser.commandeList.length) {
-          observer.error("Cannot remove product. Order doesnt exists");
-          return
-        }
-
-        const command = updatedUser.commandeList[0];
-
-        const productFound = command.ligneCommandeList.find(p => p.id === id);
-        if (productFound) {
-          productFound.quantite--;
-          if (productFound.quantite === 0) {
-            command.ligneCommandeList.splice(command.ligneCommandeList.indexOf(productFound), 1);
-          }
-        } else {
-          delete command.ligneCommandeList[id];
-        }
-
-        this.updatePriceTotal(command);
-
-
-        this.user.next(updatedUser);
-        observer.next(updatedUser);
-        observer.complete();
-      }, error => {
-        observer.error("Cannot remove product");
-      });
-    });
-  }
-
-  removeAllProduct() {
-    return new Observable(observer => { 
-      this.user.subscribe(user => {
-        const updatedUser = JSON.parse(JSON.stringify(user));
-        
-        if(!updatedUser.commandeList.length) {
-          observer.error("Cannot remove products. Order doesnt exists");
-          return
-        }
-
-        updatedUser.commandeList = [];
-
-        this.user.next(updatedUser);
-        observer.next(updatedUser);
-        observer.complete();
-      }, error => {
-        observer.error("Cannot remove products");
-      });
-    });
-  }
-
-  updatePriceTotal(order: any) {
-     return new Observable(observer => { 
-      this.user.subscribe(user => {
-        const updatedUser = JSON.parse(JSON.stringify(user));
-        
-        if(!updatedUser.commandeList.length) {
-          observer.error("Cannot remove products. Order doesnt exists");
-          return
-        }
-
-        order.prix_total = order.ligneCommandeList.reduce(function(accumulator, currentItem) {
-          return accumulator + currentItem.prix_unitaire;
-        }, 0);
-        
-        this.user.next(updatedUser);
-        observer.next(updatedUser);
-        observer.complete();
-      }, error => {
-        observer.error("Cannot remove products");
-      });
-    });
   }
 
 }
